4efc271b799c78a89353446dab6d3954cbcc6289
Liang Jiang <jal@mail.ustc.edu.cn>
2016-04-05 19:48:30 +0800

    compplete code to push projects configed in file

--- push_project.py
+++ push_project.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 import ConfigParser
 import argparse
+import os
+import subprocess
 
 def argparser():
     parser = argparse.ArgumentParser()
-    parser.add("-f", "--config", help = "config file",
-            action = "store", default = 'conf/push_projects.conf')
+    parser.add_argument("-f", "--config", help = "config file",
+            action = "store", default = 'conf/push_project.conf')
     return parser
 def main():
     #parse command line arguments
@@ -16,8 +18,18 @@ def main():
     #parse configurations from file
     cp = ConfigParser.ConfigParser()
     cp.read(config)
-    sections = cp.sections()
-    print "sections: ", sections, type(secs)
-    
+    secs = cp.sections()
+    for sec in secs:
+        path = cp.get(sec, "path")
+        os.chdir(path)
+        child = subprocess.Popen("git pull", shell = True)
+        child.wait()
+        child = subprocess.Popen("echo " + path 
+                + "pull >> /home/liangjiang/code/scripts/out", shell = True)
+        child = subprocess.Popen("git push", shell = True)
+        child = subprocess.Popen("echo " + path 
+                + "push >> /home/liangjiang/code/scripts/out", shell = True)
+        child.wait()
+
 if __name__ == "__main__":
     main()
